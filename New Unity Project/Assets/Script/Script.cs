e277365785764080d059fb78d925d76209e75828
qxzxy <2670324642@qq.com>
2021-04-26 20:25:11 +0800

    work

---
+++ New Unity Project/Assets/Script/Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D herobody;
+    public float force = 100;
+    void Start()
+    {
+        herobody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //if
+        herobody.AddForce(Vector2.right * h * force);
+    }
+}
